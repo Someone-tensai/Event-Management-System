705f93ee4eda43cd64c79f2cdefb55bbba9e2afc
Someone-tensai <paudelsulav5@gmail.com>
2026-01-19 20:11:41 +0545

    Event Routes Except Edit
    Added event routes except to edit them

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,10 @@ dotenv.config();
 
 const express = require("express");
 const app = express();
-
+app.use(express.json());
 const events_router = require("./routes/events");
 
-app.use("/events", events_router);
+app.use("/api/events", events_router);
 
 const PORT = 3000;
 app.listen(PORT, (error)=>{
